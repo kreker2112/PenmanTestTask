392268ba0c862da70941ecea48506228d5ff3c50
kreker2112 <kreker2112@gmail.com>
2024-05-10 01:08:28 +0300

    some changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+  base: '/drone.github.io',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-});
+  }
+})
